84b8f940205e94600851141ef3212c2e733e4890
lokesh182 <lokeshbapte.18@gmail.com>
2025-01-29 22:55:34 +0530

    New Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,20 +79,21 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
+    let projectCategory = filterItems[i].dataset.category.toLowerCase(); // Convert to lowercase
 
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].style.display = "block";
+    } else if (selectedValue === projectCategory) {
       filterItems[i].classList.add("active");
+      filterItems[i].style.display = "block";
     } else {
       filterItems[i].classList.remove("active");
+      filterItems[i].style.display = "none";
     }
-
   }
-
-}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
